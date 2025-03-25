7b60bb4b03161a49f9db8caa2ea515972ebc0e51
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-03-25 13:31:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ search_tool = Tool(
 
 ## Streamlit Application
 st.title("🔎 Langchain - Chat with Search")
-"""
-In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
-"""
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
